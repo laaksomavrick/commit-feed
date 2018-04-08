44a490f7e386fc055464c1aca5a11b74f56e7293
mlaakso@dev <laakso.mavrick@gmail.com>
2018-04-08 15:38:05 -0400

    card hover transition

--- client/src/components/BoardGridCard/BoardGridCard.js
+++ client/src/components/BoardGridCard/BoardGridCard.js
@@ -6,15 +6,47 @@ import PropTypes from 'prop-types'
 import Card, { CardContent } from 'material-ui/Card'
 import Typography from 'material-ui/Typography'
 
-const BoardGridCard = ({ board }) => (
-    <Card>
-      <CardContent>
-        <Typography variant="headline" component="h2">
-          {board.name}
-        </Typography>
-      </CardContent>
-    </Card>
-)
+import './BoardGridCard.css'
+
+class BoardGridCard extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      raised: false
+    }
+  }
+
+  onMouseEnter = () => {
+    this.setState({ raised: true })
+  }
+
+  onMouseLeave = () => {
+    this.setState({ raised: false })
+  }
+
+  render() {
+
+    const { board } = this.props
+    const { raised } = this.state
+
+    return (
+      <div className="board-grid-card-container">
+        <Card 
+          raised={raised}
+          onMouseEnter={this.onMouseEnter}
+          onMouseLeave={this.onMouseLeave}
+        >
+          <CardContent>
+            <Typography variant="headline" component="h2">
+              {board.name}
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
+    )
+  }
+}
 
 BoardGridCard.propTypes = {
   board: PropTypes.object.isRequired
